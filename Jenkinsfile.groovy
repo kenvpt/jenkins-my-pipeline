d08db2795862f13e1bd3f88909ccd3f9951984de
vkenpt <johndoe@example.com>
2020-07-12 18:05:46 -0500

    add pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,14 @@
-
+node{
+    stage("Pull Repo"){
+        echo "hello"   
+    }
+    stage("Build Image"){
+        sh "packer version"
+    }
+    stage("Send Notification to Slack"){
+        echo "hello"
+    }
+    stage("Send Email"){
+        echo "hello"
+    }
+}
